b2c0deef81291cdf06ee6993f982258b3e6b9d1d
amuchina <76793200+amuchina@users.noreply.github.com>
2022-09-30 14:43:03 +0200

    Add files via upload

---
+++ Game/assets/tileset-mappa/bedroomTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="bedroomTileset" tilewidth="52" tileheight="52" tilecount="36" columns="9">
+ <image source="bedroomTileset.png" width="468" height="208"/>
+</tileset>
